e8a5a7576dc4d765af6cb6fb2dbe0a42d2d5dcdc
Guyo <guyo.halake@students.moringaschool.com>
2023-11-17 12:06:51 +0300

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import { HiArrowNarrowRight } from 'react-icons/hi'
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
 
 function Home() {
   return (
-    <div name='home' className='w-full h-screen bg-black '>
-
-        {/* container */}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-[#646881]'>
-            <p className='text-[#e93f33] text-xl'> Hi, my name is</p>
-            <h1 className='text-4xl sm:text-7xl font-bold text-[#968d79]'>Guyo Halake</h1>
-            <h2 className='text-4xl sm:text-7xl font-bold text-[#1C202D]'>I'm a Software Engineering Student</h2>
-            <p className='py-4 max-w-[700px]'> I’m a Software engineering student specializing in building (and occasionally
+    <div id='home' className='w-full h-screen bg-black '>
+      {/* container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full text-[#646881]'>
+        <p className='text-[#e93f33] text-xl'> Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#968d79]'>Guyo Halake</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#1C202D]'>I'm a Software Engineering Student</h2>
+        <p className='py-4 max-w-[700px]'> I’m a Software engineering student specializing in building (and occasionally
           designing) exceptional digital experiences. Currently, I’m focused on
           building responsive full-stack web applications.</p>
-          <div>
-            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#e93f33]'>View Work 
-            <span className='group-hover:rotate-90 duration-300'>
-            <HiArrowNarrowRight className='ml-3'/>
-            </span>
+        <div>
+          {/* Link to navigate to the 'work' section */}
+          <Link to='work' smooth={true} duration={500}>
+            <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#e93f33]'>
+              View Work
+              <span className='group-hover:rotate-90 duration-300'>
+                <HiArrowNarrowRight className='ml-3' />
+              </span>
             </button>
-          </div>
+          </Link>
         </div>
-     </div>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -10,11 +10,11 @@ import TailWind from '../data/tailwind.png';
 
 function Skill() {
   return (
-    <div name='skills' className='bg-black w-full h-screen text-[#968d79]'>
+    <div id='skills' className='bg-black w-full h-screen text-[#968d79]'>
         {/* container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
-                <p className='text-4xl font-bold inline border-b-4 border-[#e93f33] py-4'>Experience</p>
+                <p className='text-4xl font-bold inline border-b-4 border-[#e93f33] py-4'>Skills</p>
                 <p className='py-8'>This are some of the technologies i have worked with:</p>
             </div>
             <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
